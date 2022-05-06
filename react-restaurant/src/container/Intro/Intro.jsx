1cf4069b14e5581cf00ffe876783f82996741cb9
Samir <Saitwalsamir@gmail.com>
2022-05-07 00:16:16 +0530

    --Intro Updated

--- react-restaurant/src/container/Intro/Intro.jsx
+++ react-restaurant/src/container/Intro/Intro.jsx
@@ -1,42 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+
 import { meal } from '../../constants';
 import './Intro.css';
 
 const Intro = () => {
-  const [playVideo, setPlayVideo] = useState(false);
+  const [playVideo, setPlayVideo] = React.useState(false);
   const vidRef = React.useRef();
-  const handleVideo = () => {
-    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
-    if (playVideo) {
-      vidRef.current.pause();
-    } else {
-      vidRef.current.play();
-    }
-  }
+
   return (
-    <div className='app__video'>
-      <vedio
-        src={meal}
+    <div className="app__video">
+      <video
         ref={vidRef}
+        src={meal}
         type="video/mp4"
         loop
         controls={false}
         muted
       />
-      <div className='app__video-overlay flex__center'>
-        <div className='app__video-overlay_circle flex__center'
-          onClick={handleVideo}
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
         >
           {playVideo ? (
-            <BsPauseFill color='#fff' fontSize={30} />
-          ) : <BsFillPlayFill color='#fff' fontSize={30} />}
-
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
         </div>
-
       </div>
     </div>
-  )
+  );
 };
 
 export default Intro;

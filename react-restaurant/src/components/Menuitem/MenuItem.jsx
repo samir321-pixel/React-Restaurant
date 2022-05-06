389c1b8c7138efb7e5aa14c85d87fefd83dd8446
Samir <Saitwalsamir@gmail.com>
2022-05-06 23:16:03 +0530

    --SpecialMenu Updated

--- react-restaurant/src/components/Menuitem/MenuItem.jsx
+++ react-restaurant/src/components/Menuitem/MenuItem.jsx
@@ -8,7 +8,16 @@ const MenuItem = ({ title, price, tags }) => (
       <div className='app__menuitem-name'>
         <p className='p__cormorant' style={{ color: '#DCCA87' }}>{title}</p>
       </div>
+      <div className='app__menuitem-dash' />
+
+      <div className='app__menuitem-price'>
+        <p className='p__cormorant'>{price}</p>
+      </div>
     </div>
+    <div className='app__menuitem-sub'>
+      <p className='p__opensans' style={{ color: '#AAA' }}>{tags}</p>
+    </div>
+
   </div>
 );
 

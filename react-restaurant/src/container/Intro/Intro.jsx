365c04076af948b2868263b9c60ad835161bbc1d
Samir <Saitwalsamir@gmail.com>
2022-05-07 00:09:26 +0530

    --Intro Design

--- react-restaurant/src/container/Intro/Intro.jsx
+++ react-restaurant/src/container/Intro/Intro.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = React.useRef();
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  }
+  return (
+    <div className='app__video'>
+      <vedio
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className='app__video-overlay flex__center'>
+        <div className='app__video-overlay_circle flex__center'
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color='#fff' fontSize={30} />
+          ) : <BsFillPlayFill color='#fff' fontSize={30} />}
+
+        </div>
+
+      </div>
+    </div>
+  )
+};
 
 export default Intro;

093561bd3c8c9b79d4e2c06f4c046dce94593065
Samir <Saitwalsamir@gmail.com>
2022-05-07 21:56:02 +0530

    --Dallery Updated

--- react-restaurant/src/container/Gallery/Gallery.jsx
+++ react-restaurant/src/container/Gallery/Gallery.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BsArrowLeftShort, BsArrowRightShort, BsInstagram } from 'react-icons/bs';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
@@ -15,33 +16,32 @@ const Gallery = () => {
     } else {
       current.scrollLeft += 300;
     }
-  }
+  };
 
-  const galleryImages=[images.gallery01, images.gallery02, images.gallery03, images.gallery04]
   return (
-    <div className='app__gallery flex__center'>
-      <div className='app__gallery-content'>
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
         <SubHeading title="Instagram" />
-        <h1 className='headtext_-cormorant'>Photo Gallery</h1>
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
         <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
-        <button type='button' className='custom__button'>View More</button>
+        <button type="button" className="custom__button">View More</button>
       </div>
-      <div className='app__gallery-images'>
-        <div className='app__gallery-images_container' ref={scrollRef}>
-          {galleryImages.map((image,index)=>(
-            <div className='app__gallery-images_card flex__center' key={`gallery_image-${index+1}`}>
-              <img src={image} alt='gallery'></img>
-              <BsInstagram className="gallery__image-icon"></BsInstagram>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
         </div>
-        <div className='app__gallery-images_arrows'>
-          <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
-          <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
     </div>
   );
+};
 
-}
 export default Gallery;

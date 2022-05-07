11ac713eea92e13f4fcf6b448248f490935e061b
Samir <Saitwalsamir@gmail.com>
2022-05-07 22:57:39 +0530

    --Footer Design

--- react-restaurant/src/components/Footer/Newsletter.jsx
+++ react-restaurant/src/components/Footer/Newsletter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter'>
+      <SubHeading title="Newsletter"></SubHeading>
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>    
+    
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder='Enter Your Email Address'></input>
+      <button type="button" className="custom__button">Subscribe</button>
+
+    </div>
   </div>
 );
 
--- react-restaurant/src/container/Findus/FindUs.jsx
+++ react-restaurant/src/container/Findus/FindUs.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 
